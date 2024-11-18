71b534bd5c7bca0a5b7fb40f12a6f3d486240311
olehhapuk <oleggapuk20@gmail.com>
2024-11-18 13:22:00 +0200

    feat: added infinite scroll for chat

--- src/features/rooms/components/chat-messages.tsx
+++ src/features/rooms/components/chat-messages.tsx
@@ -1,20 +1,19 @@
 import { Fragment, useEffect } from 'react';
 import { socket } from '@/lib/socket';
-import { Button } from '@/components/ui/button';
 import { useMessages } from '../hooks/use-messages';
 import ChatMessage from './chat-message';
 import { Message } from '@/types/message';
 import { appendMessage } from '../utils/append-message';
 import { useQueryClient } from '@tanstack/react-query';
 import ChatMessagesSkeletons from './chat-messages-skeletons';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface ChatMessagesProps {
   roomId: string;
 }
 
 export default function ChatMessages({ roomId }: ChatMessagesProps) {
-  const { data, isFetchingNextPage, isLoading, hasNextPage, fetchNextPage } =
-    useMessages(roomId);
+  const { data, isLoading, hasNextPage, fetchNextPage } = useMessages(roomId);
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -29,8 +28,28 @@ export default function ChatMessages({ roomId }: ChatMessagesProps) {
     };
   }, [queryClient]);
 
+  const dataLength =
+    data?.pages.reduce((prev, page) => prev + page.data.length, 0) ?? 0;
+
   return (
-    <div className="flex flex-col-reverse gap-1 px-3 pt-3">
+    <InfiniteScroll
+      dataLength={dataLength}
+      hasMore={hasNextPage}
+      loader={
+        <p className="text-center text-muted-foreground text-sm py-2">
+          Loading...
+        </p>
+      }
+      next={fetchNextPage}
+      inverse
+      scrollableTarget="scrollable"
+      className="flex flex-col-reverse gap-1 px-2 pt-2"
+      endMessage={
+        <p className="text-center text-muted-foreground text-sm py-2">
+          No messages left
+        </p>
+      }
+    >
       {isLoading && <ChatMessagesSkeletons />}
       {!isLoading &&
         data?.pages.map((page, i) => (
@@ -40,20 +59,6 @@ export default function ChatMessages({ roomId }: ChatMessagesProps) {
             ))}
           </Fragment>
         ))}
-
-      {hasNextPage && (
-        <div className="flex justify-center items-center p-2">
-          <Button
-            type="button"
-            onClick={() => fetchNextPage()}
-            size="sm"
-            variant="secondary"
-            disabled={isFetchingNextPage}
-          >
-            {isFetchingNextPage ? 'Loading...' : 'Load more'}
-          </Button>
-        </div>
-      )}
-    </div>
+    </InfiniteScroll>
   );
 }
--- src/features/rooms/hooks/use-messages.ts
+++ src/features/rooms/hooks/use-messages.ts
@@ -2,25 +2,23 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { getRoomMessagesService } from '../services/get-room-messages.service';
 
 export function useMessages(roomId: string) {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
-    useInfiniteQuery({
-      queryKey: ['rooms', roomId, 'messages'],
-      queryFn: ({ pageParam }) => getRoomMessagesService(roomId, pageParam),
-      initialPageParam: undefined as
-        | {
-            createdAt: string;
-            id: string;
-          }
-        | undefined,
-      getNextPageParam: (lastPage) =>
-        lastPage.hasNextPage ? lastPage.nextCursor : undefined,
-    });
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
+    queryKey: ['rooms', roomId, 'messages'],
+    queryFn: ({ pageParam }) => getRoomMessagesService(roomId, pageParam),
+    initialPageParam: undefined as
+      | {
+          createdAt: string;
+          id: string;
+        }
+      | undefined,
+    getNextPageParam: (lastPage) =>
+      lastPage.hasNextPage ? lastPage.nextCursor : undefined,
+  });
 
   return {
     data,
     hasNextPage,
     fetchNextPage,
-    isFetchingNextPage,
     isLoading,
   };
 }
