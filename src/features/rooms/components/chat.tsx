7047c4a05c0a575e17639808af23c06918bb3cfe
Oleh Hapuk <oleggapuk20@gmail.com>
2024-12-04 14:15:21 +0200

    feat: added scroll to bottom button (#11)

--- src/features/rooms/components/chat.tsx
+++ src/features/rooms/components/chat.tsx
@@ -1,6 +1,9 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import ChatEditor from './chat-editor';
 import ChatMessages from './chat-messages';
+import { Button } from '@/components/ui/button';
+import { ChevronDown } from 'lucide-react';
+import { AnimatePresence, motion } from 'motion/react';
 
 interface ChatProps {
   roomId: string;
@@ -8,14 +11,17 @@ interface ChatProps {
 
 export default function Chat({ roomId }: ChatProps) {
   const chatMessagesScrollRef = useRef<HTMLDivElement>(null);
+  const [isScrollToBottomVisible, setIsScrollToBottomVisible] = useState(false);
+
+  const scrollToBottom = () => {
+    chatMessagesScrollRef.current?.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
   const handleMessageSend = () => {
-    setTimeout(() => {
-      chatMessagesScrollRef.current?.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-    }, 0);
+    setTimeout(scrollToBottom, 0);
   };
 
   return (
@@ -24,8 +30,39 @@ export default function Chat({ roomId }: ChatProps) {
         ref={chatMessagesScrollRef}
         className="grow overflow-y-auto flex flex-col-reverse"
         id="scrollable"
+        onScroll={(e) =>
+          setIsScrollToBottomVisible(e.currentTarget.scrollTop < -300)
+        }
       >
         <ChatMessages roomId={roomId} />
+        <AnimatePresence>
+          {isScrollToBottomVisible && (
+            <Button
+              size="icon"
+              type="button"
+              className="fixed bottom-36 right-2 rounded-full"
+              onClick={scrollToBottom}
+              asChild
+            >
+              <motion.button
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                }}
+                transition={{
+                  duration: 0.1,
+                }}
+              >
+                <ChevronDown />
+              </motion.button>
+            </Button>
+          )}
+        </AnimatePresence>
       </div>
       <ChatEditor roomId={roomId} onSend={handleMessageSend} />
     </div>
