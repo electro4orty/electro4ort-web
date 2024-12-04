f4ad23a1dcb4969933888cf802632040eb96efc2
olehhapuk <oleggapuk20@gmail.com>
2024-12-04 19:53:38 +0200

    feat: added chat editor preview

--- src/features/rooms/components/editor.tsx
+++ src/features/rooms/components/editor.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import DOMPurify from 'dompurify';
+import { marked } from 'marked';
+import { Edit2, Eye } from 'lucide-react';
 
 interface EditorProps {
   value: string;
@@ -15,6 +19,7 @@ export default function Editor({
   onEnter,
 }: EditorProps) {
   const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
 
   useEffect(() => {
     const input = inputRef.current;
@@ -23,8 +28,8 @@ export default function Editor({
     }
 
     input.style.height = '0px';
-    input.style.height = `${input.scrollHeight + 2}px`;
-  }, [value]);
+    input.style.height = `${input.scrollHeight + 3}px`;
+  }, [value, isPreviewVisible]);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (!e.shiftKey && e.code === 'Enter') {
@@ -34,16 +39,45 @@ export default function Editor({
   };
 
   return (
-    <div>
-      <Textarea
-        ref={inputRef}
-        disabled={disabled}
-        placeholder="Write a message"
-        value={value}
-        onChange={(e) => onChange(e.currentTarget.value)}
-        onKeyDown={handleKeyDown}
-        className="max-h-[200px] resize-none"
-      />
+    <div className="flex gap-1 items-end grow">
+      {isPreviewVisible ? (
+        <div
+          className="bg-secondary w-full px-3 py-[7px] border rounded-md markdown max-h-[200px] overflow-y-auto"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(
+              marked.parse(value, {
+                async: false,
+              }),
+              {
+                ADD_ATTR: ['target'],
+              }
+            ),
+          }}
+        />
+      ) : (
+        <Textarea
+          ref={inputRef}
+          disabled={disabled}
+          placeholder="Write a message"
+          value={value}
+          onChange={(e) => onChange(e.currentTarget.value)}
+          onKeyDown={handleKeyDown}
+          className="max-h-[200px] resize-none"
+        />
+      )}
+      <Button
+        type="button"
+        size="icon"
+        variant="secondary"
+        onClick={() => setIsPreviewVisible((prev) => !prev)}
+        className="shrink-0"
+      >
+        {isPreviewVisible ? (
+          <Edit2 className="size-4" />
+        ) : (
+          <Eye className="size-4" />
+        )}
+      </Button>
     </div>
   );
 }
