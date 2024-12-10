6bdabd106464df9da6306943f478877b73fc6036
olehhapuk <oleggapuk20@gmail.com>
2024-12-10 14:12:05 +0200

    feat: added attachments carousel

--- src/features/rooms/components/message/attachments.tsx
+++ src/features/rooms/components/message/attachments.tsx
@@ -1,40 +1,44 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { Attachment } from '@/types/attachment';
 import { getFileUrl } from '@/utils/get-file-url';
+import AttachmentPreviewDialog from './attachment-preview-dialog';
+import { useState } from 'react';
 
 interface AttachmentsProps {
   attachments: Attachment[];
 }
 
 export default function Attachments({ attachments }: AttachmentsProps) {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
   return (
     <div className="flex flex-wrap gap-1">
-      {attachments.map((attachment) => (
-        <Dialog key={attachment.fileName}>
-          <DialogTrigger>
-            <img
-              src={getFileUrl(attachment.fileName)}
-              alt={attachment.fileName}
-              className="block size-40 object-cover object-center overflow-hidden rounded-lg"
-            />
-          </DialogTrigger>
-          <DialogContent>
-            <DialogTitle>Preview</DialogTitle>
-            <DialogDescription className="sr-only">Preview</DialogDescription>
-            <img
-              src={getFileUrl(attachment.fileName)}
-              alt={attachment.fileName}
-              className="rounded"
-            />
-          </DialogContent>
-        </Dialog>
+      {attachments.map((attachment, i) => (
+        <button
+          key={attachment.fileName}
+          type="button"
+          onClick={() => setSelectedIndex(i)}
+        >
+          <img
+            src={getFileUrl(attachment.fileName)}
+            alt={attachment.fileName}
+            className="block size-40 object-cover object-center overflow-hidden rounded-lg"
+          />
+        </button>
       ))}
+      <Dialog
+        open={selectedIndex !== null}
+        onOpenChange={(value) => setSelectedIndex(value ? 0 : null)}
+      >
+        <DialogContent>
+          {selectedIndex !== null && (
+            <AttachmentPreviewDialog
+              attachments={attachments}
+              selectedIndex={selectedIndex}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
