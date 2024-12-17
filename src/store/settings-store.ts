9bf29322fa1ed5180bd50eef7cb02d911be7df97
Oleh Hapuk <oleggapuk20@gmail.com>
2024-12-17 22:07:57 +0200

    feat: added theme support

--- src/store/settings-store.ts
+++ src/store/settings-store.ts
@@ -4,6 +4,8 @@ import { persist } from 'zustand/middleware';
 interface SettingsStore {
   isSendMessageUsingModifier: boolean;
   setIsSendMessageUsingModifier: (value: boolean) => void;
+  theme: string;
+  setTheme: (value: string) => void;
 }
 
 export const useSettingsStore = create<SettingsStore>()(
@@ -12,9 +14,18 @@ export const useSettingsStore = create<SettingsStore>()(
       isSendMessageUsingModifier: false,
       setIsSendMessageUsingModifier: (value) =>
         set({ isSendMessageUsingModifier: value }),
+      theme: 'default',
+      setTheme: (value) => {
+        set({ theme: value });
+        document.documentElement.className = value === 'default' ? '' : value;
+      },
     }),
     {
       name: 'settings-store',
+      onRehydrateStorage: () => (state) => {
+        document.documentElement.className =
+          !state || state.theme === 'default' ? '' : state.theme;
+      },
     }
   )
 );
